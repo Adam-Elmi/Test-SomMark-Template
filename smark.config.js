50b25756b1e2da4609a3fbb04cf73a3df7de4c1f
Adam-Elmi <Adamcade123@gmail.com>
2026-06-04 20:51:13 +0300

    feat: configure smark.config.js with placeholders, importAliases, customProps, removeComments

---
+++ smark.config.js
@@ -0,0 +1,11 @@
+export default {
+  placeholders: {
+    siteName: "TestSite",
+    version: "2.0.0",
+  },
+  importAliases: {
+    "@/": "src/",
+  },
+  customProps: ["theme"],
+  removeComments: false,
+};
